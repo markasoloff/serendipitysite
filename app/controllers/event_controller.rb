f33dd1a0f8479185d16a4863dec786f69d14aba0
“Marksoloff” <marksoloff@gmail.com>
2019-04-07 21:31:49 -0500

    added event model & skull tags

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,2 +1,6 @@
 class EventController < ApplicationController
+  def index
+    @events = Event.all 
+    render 'event/index.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  root "event#index"
+
 end
